257e6aa1a5f851a5c03605dd47a74a723faff130
PhilippeRobichaudGionet <prg@hotmail.ca>
2025-04-11 12:59:32 -0400

    AddTexinput et WebSupport

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder='Nom' value={Text} style={styles.input}/>
+      <TextInput placeholder='Mot de passe' value={Text} style={styles.input}/>
     </View>
   );
 }
@@ -17,4 +17,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
 });
